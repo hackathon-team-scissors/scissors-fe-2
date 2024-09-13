290ee35c8c554d309a707ba9774b4f0a806bd8c7
Laura van Oosterom <laura.van.oosterom@nl.abnamro.com>
2024-09-13 12:18:31 +0200

    add profile page

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,20 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { RouterView } from 'vue-router'
-import { useTokenStore } from './stores/tokenManager';
-import router from './router';
+import { onMounted } from "vue";
+import { RouterView } from "vue-router";
+import { useTokenStore } from "./stores/tokenManager";
+import router from "./router";
 
 const tokenStore = useTokenStore();
 
-onMounted(()=>{
-  if (!tokenStore.hasToken()){
-    router.push('/login');
+onMounted(() => {
+  if (!tokenStore.hasToken()) {
+    router.push("/login");
   } else {
-    router.push('/emissions');
+    router.push("/profile");
   }
-})
-
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
